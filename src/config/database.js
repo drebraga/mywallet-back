b2fc77014bca0ae08f79be3be6d4e3cb59635256
drebraga <dre.braga@gmail.com>
2023-01-20 15:00:21 -0300

    test: deploy test

--- src/config/database.js
+++ src/config/database.js
@@ -2,23 +2,27 @@ import { MongoClient, ServerApiVersion } from "mongodb";
 import dotenv from "dotenv";
 
 dotenv.config();
-
+const mongoClient = new MongoClient(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1
+});
 let db;
 
-try {
-    console.log("OK");
-    const mongoClient = new MongoClient(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        serverApi: ServerApiVersion.v1
-    });
+// try {
+//     await mongoClient.connect();
+//     db = mongoClient.db("mywallet");
+// } catch (err) {
+//     console.log(err);
+// }
 
-    await mongoClient.connect();
-    console.log("OK1");
-    db = mongoClient.db("mywallet");
-    console.log("OK2");
-} catch (err) {
+
+mongoClient.connect(err => {
+  db = client.db("mywallet");
+  if (err) {
     console.log(err);
-}
+    mongoClient.close();
+  }
+});
 
 export default db;
